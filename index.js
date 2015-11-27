c18d2511a188a534f1fd688336147804c1c9efd0
hangyang.wang <w.why@163.com>
2015-11-27 11:07:01 +0800

    options.root add defined value
    默认使用express中的模版路径为root路径.

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var path = require("path");
 function expressify(options){
   options = options || [];
   return function render( filename, data , callback ) {
-    var engine = new vm.Engine({
-      template: filename,
-      root: options.root ||[]
-    });
-    callback(null, engine.render( data ));
+    try{
+	    var engine = new vm.Engine({
+	      template: filename,
+	      root: options.root || data.settings.views || []
+	    });
+	    callback(null,engine.render( data ));
+	}catch(e){
+    	callback(e);
+	}
   }
 }
 
